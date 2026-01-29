b5cbb67a84c92817930e1204fea6fec35c0f5da8
Huda Khan <hudakhan122211@gmail.com>
2026-01-29 19:36:27 +0500

    Refactor: Comment out grading logic and retain weather functionality

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
- let score = 50;
-        let grade;
-        if (score >= 90) {
-            grade = 'A';
-    }else if (score >= 80){
-        grade = 'B';
-    } else if (score >= 70){
-        grade ='C';
-    } else if (score >= 60){
-        grade = 'D';
-    }else{
-        grade = 'F';
+//  let score = 50;
+//         let grade;
+//         if (score >= 90) {
+//             grade = 'A';
+//     }else if (score >= 80){
+//         grade = 'B';
+//     } else if (score >= 70){
+//         grade ='C';
+//     } else if (score >= 60){
+//         grade = 'D';
+//     }else{
+//         grade = 'F';
+//     }
+//     console.log('Your grade is'+ " " +grade)
+
+
+
+    let temperature = 50;
+    let weather;
+    if (temperature >= 30) {
+        weather = 'Hot';
+    } else if (temperature >= 20) {
+        weather = 'Warm';
+    } else if (temperature >= 10) {
+        weather = 'Cool';
+    } else {
+        weather = 'Cold';
     }
-    console.log('Your grade is'+ " " +grade)
+    console.log('The weather is' + " " + weather);
