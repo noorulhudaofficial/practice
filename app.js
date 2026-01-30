f0b0ab6f37a081f944599777b08b250c8fb423ac
Huda Khan <hudakhan122211@gmail.com>
2026-01-30 19:11:17 +0500

    Js added

--- app.js
+++ app.js
@@ -15,15 +15,36 @@
 
 
 
-    let temperature = 50;
-    let weather;
-    if (temperature >= 30) {
-        weather = 'Hot';
-    } else if (temperature >= 20) {
-        weather = 'Warm';
-    } else if (temperature >= 10) {
-        weather = 'Cool';
+    // let temperature = 50;
+    // let weather;
+    // if (temperature >= 30) {
+    //     weather = 'Hot';
+    // } else if (temperature >= 20) {
+    //     weather = 'Warm';
+    // } else if (temperature >= 10) {
+    //     weather = 'Cool';
+    // } else {
+    //     weather = 'Cold';
+    // }
+    // console.log('The weather is' + " " + weather);
+
+    let day = 3;
+    let dayName;
+    if (day === 1) {
+        dayName = 'Monday';
+    } else if (day === 2) {
+        dayName = 'Tuesday';
+    } else if (day === 3) {
+        dayName = 'Wednesday';
+    } else if (day === 4) {
+        dayName = 'Thursday';
+    } else if (day === 5) {
+        dayName = 'Friday';
+    } else if (day === 6) {
+        dayName = 'Saturday';
+    } else if (day === 7) {
+        dayName = 'Sunday';
     } else {
-        weather = 'Cold';
+        dayName = 'Invalid day';
     }
-    console.log('The weather is' + " " + weather);
+    console.log('The day is' + " " + dayName);
