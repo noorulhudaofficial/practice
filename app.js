1be5be01164a2dcca279a583733ac2f445ecace5
Huda Khan <hudakhan122211@gmail.com>
2026-02-05 20:13:15 +0500

    Increment and decrement counter

--- app.js
+++ app.js
@@ -1,50 +1,9 @@
-//  let score = 50;
-//         let grade;
-//         if (score >= 90) {
-//             grade = 'A';
-//     }else if (score >= 80){
-//         grade = 'B';
-//     } else if (score >= 70){
-//         grade ='C';
-//     } else if (score >= 60){
-//         grade = 'D';
-//     }else{
-//         grade = 'F';
-//     }
-//     console.log('Your grade is'+ " " +grade)
-
-
-
-    // let temperature = 50;
-    // let weather;
-    // if (temperature >= 30) {
-    //     weather = 'Hot';
-    // } else if (temperature >= 20) {
-    //     weather = 'Warm';
-    // } else if (temperature >= 10) {
-    //     weather = 'Cool';
-    // } else {
-    //     weather = 'Cold';
-    // }
-    // console.log('The weather is' + " " + weather);
-
-    let day = 3;
-    let dayName;
-    if (day === 1) {
-        dayName = 'Monday';
-    } else if (day === 2) {
-        dayName = 'Tuesday';
-    } else if (day === 3) {
-        dayName = 'Wednesday';
-    } else if (day === 4) {
-        dayName = 'Thursday';
-    } else if (day === 5) {
-        dayName = 'Friday';
-    } else if (day === 6) {
-        dayName = 'Saturday';
-    } else if (day === 7) {
-        dayName = 'Sunday';
-    } else {
-        dayName = 'Invalid day';
-    }
-    console.log('The day is' + " " + dayName);
+let count = 0;
+function increment(){
+    count++ ;
+    document.getElementById("count").textContent=count;
+}
+function decrement(){
+    count-- ;
+    document.getElementById("count").textContent=count;
+}
